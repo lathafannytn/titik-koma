16a5357dbf1837a91d0cd4353dacd583460fae43
Lathafanny <126649660+lathafannytn@users.noreply.github.com>
2024-04-23 21:18:58 +0700

    1

--- lib/screen/dashboard/home.dart
+++ lib/screen/dashboard/home.dart
@@ -2,144 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:tikom/screen/widget/appwidget.dart';
-// import 'package:tikom/screen/widget/callcenterwidget.dart';
-// import 'package:tikom/screen/widget/popularwidget.dart';
-// import 'package:tikom/screen/widget/promowidget.dart';
-
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: ListView(
-//         children: [
-//           AppWidget(),
-//           Padding(
-//             padding: EdgeInsets.symmetric(
-//               vertical: 10.0,
-//               horizontal: 15.0,
-//             ),
-//             child: Row(
-//               children: [
-//                 Expanded(
-//                   child: Container(
-//                     height: 50.0,
-//                     decoration: BoxDecoration(
-//                       color: Color(0xFFF3F3F3),
-//                       borderRadius: BorderRadius.circular(10.0),
-//                       boxShadow: [
-//                         BoxShadow(
-//                           color: Colors.grey.withOpacity(0.5),
-//                           spreadRadius: 2.0,
-//                           blurRadius: 5.0,
-//                           offset: Offset(0, 1.0),
-//                         ),
-//                       ],
-//                     ),
-//                     child: Padding(
-//                       padding: EdgeInsets.symmetric(horizontal: 10.0),
-//                       child: TextFormField(
-//                         decoration: InputDecoration(
-//                           hintText: "What would you like to have?",
-//                           border: InputBorder.none,
-//                           prefixIcon: Icon(
-//                             CupertinoIcons.search,
-//                             color: Colors.grey,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//                 SizedBox(
-//                     width:
-//                         10), // Spacer untuk memberikan jarak antara TextFormField dan InkWell
-//                 InkWell(
-//                   onTap: () {},
-//                   child: Container(
-//                     padding: EdgeInsets.all(15),
-//                     decoration: BoxDecoration(
-//                       color: Color.fromRGBO(68, 208, 145, 1.0),
-//                       borderRadius: BorderRadius.circular(10.0),
-//                     ),
-//                     child: Icon(
-//                       Icons.filter_list,
-//                       color: Colors.black,
-//                       size: 28.0,
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Padding(
-//             padding: EdgeInsets.only(
-//               top: 20.0,
-//               left: 10.0,
-//             ),
-//             child: Text(
-//               "Lets find your best favorite service!",
-//               style: TextStyle(
-//                 fontWeight: FontWeight.w900,
-//                 fontSize: 30.0,
-//               ),
-//             ),
-//           ),
-//           PromoWidget(),
-//           Padding(
-//             padding: EdgeInsets.only(
-//               top: 20.0,
-//               left: 10.0,
-//               right: 10.0,
-//             ),
-//             child: Row(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Expanded(
-//                   child: Text(
-//                     "Popular",
-//                     style: TextStyle(
-//                       fontWeight: FontWeight.w800,
-//                       fontSize: 20.0,
-//                     ),
-//                   ),
-//                 ),
-//                 Spacer(),
-//                 GestureDetector(
-//                   onTap: () {
-//                     // Tambahkan aksi yang diinginkan saat teks diklik
-//                   },
-//                   child: Text(
-//                     "see all",
-//                     style: TextStyle(
-//                       fontWeight: FontWeight.w700,
-//                       fontSize: 13.0,
-//                       color: Colors.orange,
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Padding(
-//             padding: EdgeInsets.only(
-//               left: 10.0,
-//               right: 10.0,
-//             ),
-//             child: Text(
-//               "See the most popular service on order",
-//               style: TextStyle(
-//                 fontSize: 10.0,
-//               ),
-//             ),
-//           ),
-//           PopularWidget(),
-//           CallCenterWidget(),
-//         ],
-//       ),
-//     );
-//   }
-// }
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -188,25 +51,22 @@ class HomePage extends StatelessWidget {
                             ),
                             RichText(
                               text: TextSpan(
-                                style: DefaultTextStyle.of(context)
-                                    .style, // inherits default style
+                                style: DefaultTextStyle.of(context).style,
                                 children: <TextSpan>[
                                   TextSpan(
                                     text: 'Hi, ',
-                                    style: TextStyle(
+                                    style: GoogleFonts.workSans(
                                       fontWeight: FontWeight.normal,
-                                      color: Colors
-                                          .white, // set text color to white
-                                      fontSize: 24, // set a larger font size
+                                      color: Colors.white,
+                                      fontSize: 24,
                                     ),
                                   ),
                                   TextSpan(
                                     text: 'Fanny',
-                                    style: TextStyle(
+                                    style: GoogleFonts.workSans(
                                       fontWeight: FontWeight.bold,
-                                      color: Colors
-                                          .white, // set text color to white
-                                      fontSize: 24, // set a larger font size
+                                      color: Colors.white,
+                                      fontSize: 24,
                                     ),
                                   ),
                                 ],
@@ -227,27 +87,6 @@ class HomePage extends StatelessWidget {
                   SizedBox(
                     height: 15,
                   ),
-                  // Padding(
-                  //   padding: const EdgeInsets.all(15),
-                  //   child: Container(
-                  //     height: 60,
-                  //     width: double.infinity,
-                  //     decoration: BoxDecoration(
-                  //         color: Color(0xFFF5F5F7),
-                  //         borderRadius: BorderRadius.circular(30)),
-                  //     child: TextField(
-                  //       cursorHeight: 20,
-                  //       autofocus: false,
-                  //       decoration: InputDecoration(
-                  //           hintText: "Cari Toko Kofi Favoritmu",
-                  //           prefixIcon: Icon(Icons.search),
-                  //           border: OutlineInputBorder(
-                  //               borderSide:
-                  //                   BorderSide(color: Colors.grey, width: 2),
-                  //               borderRadius: BorderRadius.circular(30))),
-                  //     ),
-                  //   ),
-                  // ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Card(
@@ -271,7 +110,7 @@ class HomePage extends StatelessWidget {
                                 SizedBox(width: 10),
                                 RichText(
                                   text: TextSpan(
-                                    style: TextStyle(
+                                    style: GoogleFonts.workSans(
                                         fontSize: 15, color: Colors.black),
                                     children: <TextSpan>[
                                       TextSpan(
@@ -279,12 +118,15 @@ class HomePage extends StatelessWidget {
                                       ),
                                       TextSpan(
                                         text: 'poin',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
+                                        style: GoogleFonts.workSans(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15,
+                                            color: Colors.black),
                                       ),
                                     ],
                                   ),
                                 ),
+
                                 SizedBox(width: 30),
                                 Icon(
                                   Icons.discount,
@@ -294,9 +136,8 @@ class HomePage extends StatelessWidget {
                                 SizedBox(width: 5),
                                 Text(
                                   '8',
-                                  style: TextStyle(
+                                  style: GoogleFonts.workSans(
                                     fontSize: 15,
-                                    fontWeight: FontWeight.bold,
                                     color: Colors.black,
                                   ),
                                 ),
