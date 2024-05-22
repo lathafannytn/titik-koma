a1cc3a2e0c443c94a8ddc77ff84c7849442c6947
Lathafanny <126649660+lathafannytn@users.noreply.github.com>
2024-05-22 23:06:45 +0700

    baru nich

--- lib/ui/screen/product/product_card.dart
+++ lib/ui/screen/product/product_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tikom/ui/screen/order/add_on.dart';
 
 class ProductCard extends StatelessWidget {
   final String name;
@@ -16,21 +18,67 @@ class ProductCard extends StatelessWidget {
     return Card(
       elevation: 0,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
+        borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
-        children: [
-          Expanded(
-            child: Image.network(imagePath, fit: BoxFit.cover),
-          ),
-          Text(name, style: TextStyle(fontWeight: FontWeight.bold)),
-          Text('\$${price.toStringAsFixed(2)}'),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.network(
+                imagePath,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Deskripsi produk di sini',
+                    style: GoogleFonts.poppins(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Rp ${price.toStringAsFixed(0)}',
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddOnScreen()),
+                );
+              },
+              icon:
+                  Icon(Icons.add_circle, color: Color.fromARGB(255, 9, 76, 58)),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-
